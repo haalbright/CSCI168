271762799f666e86af8309611e9e0065891e5a87
maldron69 <93166655+maldron69@users.noreply.github.com>
2021-11-16 16:20:47 -0800

    Add files via upload

--- Project1/RayTracer.cpp
+++ Project1/RayTracer.cpp
@@ -1,14 +1,14 @@
 #include "RayTracer.h"
 
-void RayTracer::clear() const {
-}
+void RayTracer::clear() const {}
 
-void RayTracer::render(const Scene& _scene) const {
+//variables we will need:
+//screen hight and width:
+int height = g_height;
+int width = g_width;
 
-
-
-
-
+void RayTracer::render(const Scene& _scene) const 
+{
   // for each column i do
   //   for each row j do
   //     generate a ray origin and direction
@@ -18,4 +18,25 @@ void RayTracer::render(const Scene& _scene) const {
   //     shade the ray intersection
   //
   // Copy framebuffer to screne
+  
+  //looping over all the pixels on the screen
+  for (int j = 0; j < height; ++j)
+  {
+	  for (int i = 0; i < width; ++i)
+	  {
+		  //generating a ray origin and direction
+		  glm::vec3 origin;
+		  glm::vec3 direction;
+		  
+		  float t = INFINITY;//closest point of intersection
+		  //finding the first intersection point of the ray
+		  if(object.intersect(origin, direction, t) && t > 0)
+		  {
+			  glm::vec3 hitPoint = origin + direction * t;
+			  glColor3f(0.3,0.3,0.3);//shading the ray intersection
+		  }
+		  
+	  }
+  }
+  //copy framebuffer to scene
 }
