257df59ea3429968676a7c1da6ac4d9a831f819c
kkuenster <63666896+kkuenster@users.noreply.github.com>
2021-11-16 20:30:38 -0800

    Add files via upload

---
+++ Project1/Shadow.h
@@ -0,0 +1,18 @@
+#ifndef _SHADOW_H_
+#define _SHADOW_H_
+
+#include "GLInclude.h"
+#include "Object.h"
+#include "Light.h"
+
+
+
+class Shadow {
+ public:
+  Shadow(){};
+ private:
+ // std::vector<Object*>mObjects;
+}
+
+
+#endif
---
+++ Project1/shadow.cpp
@@ -0,0 +1,14 @@
+// dependencies 
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include "Shadow.h"
+#include "Object.h" 
+#include "Light.h"
+#include "Sphere.h"
+#include "GLInclude.h"
+
+
+void Shadow::clear() const {
+    
+}
