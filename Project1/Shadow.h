dfe424fa39aedef62f66598cc72dfdb511dae068
kkuenster <63666896+kkuenster@users.noreply.github.com>
2021-11-29 12:28:08 -0800

    Update Shadow.h

--- Project1/Shadow.h
+++ Project1/Shadow.h
@@ -1,21 +1,21 @@
 #ifndef _SHADOW_H_
 #define _SHADOW_H_
-
 #include "GLInclude.h"
 #include "Object.h"
 #include "Light.h"
 #include "Collision.h"
 #include "Ray.h"
+#include "Light.h"
 
-
-class Shadow {
-  private:
- Ray();
- Collision();
+class Shadow 
+{
+public:
+     Shadow(); Ray(); Collision();
  // std::vector<Object*>mObjects;
-  public:
-   Shadow(){};
+ 
+private:
+     glm::vec3 position;
 
-
+};
 
 #endif
