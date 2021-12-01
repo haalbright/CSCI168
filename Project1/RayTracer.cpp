64cfffb61ccd56296d6356af8d6904d8b88f82d1
maldron69 <93166655+maldron69@users.noreply.github.com>
2021-12-01 15:22:16 -0800

    Update RayTracer.cpp

--- Project1/RayTracer.cpp
+++ Project1/RayTracer.cpp
@@ -6,6 +6,7 @@ void RayTracer::clear() const {}
 //screen hight and width:
 int height = g_height;
 int width = g_width;
+int numObjects = 1;
 
 void RayTracer::render(const Scene& _scene) const 
 {
@@ -24,19 +25,20 @@ void RayTracer::render(const Scene& _scene) const
   {
 	  for (int i = 0; i < width; ++i)
 	  {
-		  //generating a ray origin and direction
-		  glm::vec3 origin;
-		  glm::vec3 direction;
+		//generating a ray origin and direction
+		glm::vec3 origin;
+		glm::vec3 direction;
 		  
-		  float t = INFINITY;//closest point of intersection
-		  //finding the first intersection point of the ray
-		  if(object.intersect(origin, direction, t) && t > 0)
-		  {
-			  glm::vec3 hitPoint = origin + direction * t;
-			  glColor3f(0.3,0.3,0.3);//shading the ray intersection
-		  }
-		  
-	  }
+		for(int k = 0; k < numObjects; ++k)
+		{
+			float t = INFINITY;//closest point of intersection
+			//finding the first intersection point of the ray
+			if(object.intersect(origin, direction, t) && t > 0)
+			{
+				glm::vec3 hitPoint = origin + direction * t;
+				g_frame[i] = glm::vec4(float(rand())/RAND_MAX, float(rand())/RAND_MAX, float(rand())/RAND_MAX, 1.f);
+			}
+		}
   }
-  //copy framebuffer to scene
+  glDrawPixels(g_width, g_height, GL_RGBA, GL_FLOAT, g_frame.get());
 }
