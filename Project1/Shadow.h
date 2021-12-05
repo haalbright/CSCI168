b6d0d138bd6f2f32f2e3f211bbee8457a6a6a20f
kkuenster <63666896+kkuenster@users.noreply.github.com>
2021-12-05 14:29:11 -0800

    Update Shadow.h

--- Project1/Shadow.h
+++ Project1/Shadow.h
@@ -11,8 +11,7 @@ class Shadow
 {
   public:
    ads_phong_lighting(const glm::vec3& r, const glm::vec3& c): ray{r}, collision{c}{}
-   glm::vec4 ray(){return ray;}
-   glm::vec4 collision(){return collision;}
+   glm::vec4 ray(){return ray;} glm::vec4 collision(){return collision;}
   private:
    glm::vec4 ray, collision;
    
